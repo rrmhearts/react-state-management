be4fed8529401ef9fa5a989e4c8eab0f71b92bbb
Ryan <rrmhearts@gmail.com>
2020-07-21 15:16:13 -0400

    general case using contact form.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Container } from "semantic-ui-react";
 
 import CounterView from "./views/counter-view";
+import ContactView from "./views/contact-view";
 
 export default function App() {
   return (
     <Container>
       <h1>React Hooks Context Demo</h1>
+      <ContactView />
       <CounterView />
     </Container>
   );
